3c5d3a3096c412e3dc584405791020f08a6b3602
gabi20grs23-maker <gabi20grs23@gmail.com>
2026-07-09 00:07:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Herramientas Online cargada correctamente");
+const buscador = document.getElementById("buscar");
+
+if (buscador) {
+    buscador.addEventListener("keyup", function () {
+
+        const texto = buscador.value.toLowerCase();
+
+        const tarjetas = document.querySelectorAll(".card");
+
+        tarjetas.forEach(function (tarjeta) {
+
+            const contenido = tarjeta.textContent.toLowerCase();
+
+            if (contenido.includes(texto)) {
+                tarjeta.style.display = "block";
+            } else {
+                tarjeta.style.display = "none";
+            }
+
+        });
+
+    });
+}
